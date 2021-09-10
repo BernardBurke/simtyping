71fd0f3ae364bef29714cad07de497f6b6a4d56e
Ben Burke <me@benburke.dev>
2021-09-11 09:28:30 +1000

    getting time imported

--- simtyping.py
+++ simtyping.py
@@ -1,5 +1,12 @@
+import time
 from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
 
 time.sleep(5)
+
+for char in "Good morning Bob. What's going on for you?":
+    keyboard.press(char)
+    keyboard.release(char)
+    time.sleep(0.12)
+
