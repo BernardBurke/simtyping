d668db4c4c9fe75b571c6c3575fed9bcbac6d6f0
Ben Burke <bernard.francis.burke@gmail.com>
2021-09-11 11:54:21 +1000

    have the countdown and most of the args

--- simtyping.py
+++ simtyping.py
@@ -1,21 +1,53 @@
 import time
+import sys
 import argparse
 from pynput.keyboard import Key, Controller
 
 parser = argparse.ArgumentParser(description='Read two input files that make up  a text conversation and send to open Windows')
-parser.add_argument('-i', '--input', dest='infile', type=file, required=True, metavar='INPUT_FILE', help='The input file')
+parser.add_argument('-i', '--input', dest='infile', type=argparse.FileType('r'), \
+    required=True, metavar='INPUT_FILE', help='The input file')
+parser.add_argument('-p1', '--person1', dest='person1',required=True, metavar='PERSON1', help='Person 1 in the conversation')
+parser.add_argument('-p2', '--person2', dest='person2',required=True, metavar='PERSON2', help='Person 2 in the conversation')
+
 
 args = parser.parse_args()
-
-infile=args.infile
-
-
 keyboard = Controller()
 
-time.sleep(50)
+def countdown(speaker, seconds):
+    sys.stdout.write(speaker + ' is about to speak')
+    for remains in range(seconds,0,-1):
+        sys.stdout.write("\r")
+        sys.stdout.write("{:2d} seconds remaining.".format(remains))
+        sys.stdout.flush
+        time.sleep(1)
 
-for char in "Good morning Bob. What's going on for you?":
-    keyboard.press(char)
-    keyboard.release(char)
-    time.sleep(0.12)
+    sys.stdout.write("\rSpeaking")
+        
+
+def sendthekeys(words, millseconds):
+    for char in words:
+        keyboard.press(char)
+        keyboard.release(char)
+        time.sleep(0.12)
+
+
+
+contents = args.infile.readlines()
+
+args.infile.close()
+
+for line in contents:
+    if args.person1 in line:
+            #print(args.person1 + ' is about to speak')
+            countdown(args.person1, 10)
+            print (line)
+            sendthekeys(line,10) 
+            
+
+
+
+
+
+
+
 
