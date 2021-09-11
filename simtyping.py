af7d5ed8229ff9bf121a80320f1b1896fc5d0606
Ben Burke <me@benburke.dev>
2021-09-11 10:12:11 +1000

    argparsing

--- simtyping.py
+++ simtyping.py
@@ -1,9 +1,18 @@
 import time
+import argparse
 from pynput.keyboard import Key, Controller
+
+parser = argparse.ArgumentParser(description='Read two input files that make up  a text conversation and send to open Windows')
+parser.add_argument('-i', '--input', dest='infile', type=file, required=True, metavar='INPUT_FILE', help='The input file')
+
+args = parser.parse_args()
+
+infile=args.infile
+
 
 keyboard = Controller()
 
-time.sleep(5)
+time.sleep(50)
 
 for char in "Good morning Bob. What's going on for you?":
     keyboard.press(char)
